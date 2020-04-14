71382043d854297b15709b7a9b81869dabdea026
lw <370588030@qq.com>
2020-04-14 22:48:49 +0800

    Initial commit

---
+++ src/main/java/Test/hello.java
@@ -0,0 +1,16 @@
+package Test;
+
+/**
+ * @author la
+ * @date 2020/4/15 - 20:37
+ */
+public class hello {
+    public void test(){
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        hello s = new hello();
+        s.test();
+    }
+}
